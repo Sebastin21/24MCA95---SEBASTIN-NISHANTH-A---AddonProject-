e8eeca10350a0c8d87762f840e3dc9c8848daae7
SEBASTIN NISHANTH A <24mca95@kgisliim.ac.in>
2025-06-01 20:57:11 +0530

    Delete src directory

--- src/StudentMain.java
+++
@@ -1,8 +0,0 @@
-public class StudentMain {
-    public static void main(String args[]){
-
-        StudentMenu SM = new StudentMenu();
-        SM.displayMainMenu();
-        System.out.println("\t\t --> END OF PROCESS <--");
-    }
-}
